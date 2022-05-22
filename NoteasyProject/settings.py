a2df45cf1e92f80e77f40474ded2b06147567b83
shreyas bansode <bansodeshreyash6@gmail.com>
2022-05-22 22:25:12 +0530

    true

--- NoteasyProject/settings.py
+++ NoteasyProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3p8r$q=ot5esb&7izalkj-7d%u#1b$s3$z!!+!5p0yn&yrd694'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
